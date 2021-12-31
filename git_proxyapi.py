b201979f2997cf3706e07a9df9c1ed3006d73ee8
e6zzseo <aqz022@qq.com>
2021-12-31 16:44:44 +0800

    git_proxyapi.py
    分别获取代理api的ip和端口

--- git_proxyapi.py
+++ git_proxyapi.py
@@ -2,6 +2,7 @@ import requests
 
 url = "http://api.xxx.cn/VAdb=1"#代理接口
 html = requests.get(url,timeout=30)
+
 ip = html.text.strip()
 proxyHost = ip[:ip.index(':')]
 proxyPort = ip[ip.index(':'):].strip(':')
